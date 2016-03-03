921471bd88f83fb568779e6e4366ca12f2c57246
JakeKeller <Ghub.Code.Jake.Keller@gmail.com>
2016-03-03 19:07:09 +0100

    Added a method to check each line of the csv

--- SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/FlightInfo.cs
+++ SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/FlightInfo.cs
@@ -22,30 +22,60 @@ namespace SheCodesMod7FlightDelaysLinqJk
             // For each row read by the streamreader run a .Add with a newly parsed object of class FlightInfo.
             // Read in a row, split the string into strings(or rather appropriate types) by comma
 
-            // List<FlightInfo> airlinePerformance2014 = new List<FlightInfo>() { };
+            int invalidLines = 0;
+
+            List<FlightInfo> airlinePerformance2014 = new List<FlightInfo>() { };
             string line = string.Empty;
 
             StreamReader reader = new StreamReader(csvFile);
 
             reader.ReadLine().Skip(1);
 
+
             while ((line = reader.ReadLine()) != null)
             {
                 string[] csvColumns = line.Split(',');
+                if (CheckIfCSVLineIsValid(csvColumns))
+                    listName.Add(new FlightInfo()
+                    {
+                        Carrier = csvColumns[0],
+                        OriginCityName = csvColumns[1],
+                        DestinationCityName = csvColumns[2],
+                        DepartureDelay = int.Parse(csvColumns[3]),
+                        ArrivalDelay = int.Parse(csvColumns[4]),
+                        Cancelled = int.Parse(csvColumns[5])
+                    });
+                else
+                    invalidLines++; // Keep these in mind! They might be why shecodes and j.archer get different results
+                // because they didn't weed out as many lines. And maybe I shouldn't weed out any information that can be used?
+                // But if one flight delay way unknown, then it doesn't make sense to divide 20min. delay by 10 flights (where only 9 
+                // included delay data), correct? This is about averages after all.
+                // Possibly include a class for debugging and csv infos where these things are stored as properties. E.g. csvAnalysis
+                
                 // Parse to class here :)
-                listName.Add(new FlightInfo()
-                {
-                    Carrier = csvColumns[0],
-                    OriginCityName = csvColumns[1],
-                    DestinationCityName = csvColumns[2],
-                    DepartureDelay = int.Parse(csvColumns[3]),
-                    ArrivalDelay = int.Parse(csvColumns[4]),
-                    Cancelled = int.Parse(csvColumns[5])
-                });
 
             }
+            Console.WriteLine(invalidLines);
             return airlinePerformance2014;
             // Idea: Make incrementing line counter and implement error message pointing to the line where an invalid entry was found
+        }
+
+        /// <summary>
+        /// Checks each line of the CSV for validity: No column except column 8 can be empty and there can be no more than 8 columns in a line.
+        /// </summary>
+        /// <param name="csvColumns"></param>
+        /// <returns></returns>
+        public static bool CheckIfCSVLineIsValid(string[] csvColumns)
+        {
+            return csvColumns.Length == 8
+                //&& csvColumns.Contains(""); // This does not work.
+                && csvColumns[0] != ""
+                && csvColumns[1] != ""
+                && csvColumns[2] != ""
+                && csvColumns[3] != ""
+                && csvColumns[4] != ""
+                && csvColumns[5] != ""
+                && csvColumns[6] != "";
         }
     }
 }
--- SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/Program.cs
+++ SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/Program.cs
@@ -13,6 +13,7 @@ namespace SheCodesMod7FlightDelaysLinqJk
             const string csvName = "airline-on-time-performance-sep2014-us.csv";
             List<FlightInfo> airlinePerformance2014 = new List<FlightInfo>() { };
             FlightInfo.PopulateListFromCSV(csvName, airlinePerformance2014);
+            Console.ReadLine();
         }
     }
 }
