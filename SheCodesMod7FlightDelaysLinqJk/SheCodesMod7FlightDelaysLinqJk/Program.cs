ab75a60cb70c24c4d46945f313920a42d8ae14a8
JakeKeller <codejakekeller@gmail.com>
2016-03-16 16:27:08 +0100

    Clean Up

--- SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/Program.cs
+++ SheCodesMod7FlightDelaysLinqJk/SheCodesMod7FlightDelaysLinqJk/Program.cs
@@ -12,14 +12,9 @@ namespace SheCodesMod7FlightDelaysLinqJk
         {
             const string csvName = "airline-on-time-performance-sep2014-us.csv";
             List<FlightInfo> airlinePerformance2014 = new List<FlightInfo>() { };
-            FlightInfo.PopulateListFromCSV(csvName, airlinePerformance2014);
-
-            /*
-            Set breakpoints to check the results for the linq statements below without having them all run after each other.
-            */
+            FlightInfo.PopulateListFromCSV(csvName, airlinePerformance2014);              
 
             // Query1. What were the arrival delays of all the flights from Boston, MA to Chicago, IL?
-            
             var delaysBostonToChicago = from flight in airlinePerformance2014
                                         where flight.OriginCityName == "Boston MA"
                                         && flight.DestinationCityName == "Chicago IL"
@@ -38,7 +33,6 @@ namespace SheCodesMod7FlightDelaysLinqJk
                                                        select flight.OriginCityName).Distinct();
 
             // Query3. What was the origin and destination of the 10 flights that had the largest arrival delay?
-            // The "select new" clause is awesome :) 
             var flightsSortedByArrivalDelayDescending = (from flight in airlinePerformance2014
                                                          orderby flight.ArrivalDelay descending
                                                          select new { flight.OriginCityName, flight.DestinationCityName, flight.ArrivalDelay }
@@ -62,15 +56,14 @@ namespace SheCodesMod7FlightDelaysLinqJk
 
             // Query6. What was the origin and destination of the shortest flight in the data set and what is the 
             // distance of that flight?
-
             var shortestFlight = (from flight in airlinePerformance2014
                                   orderby flight.Distance
                                   select new { flight.Distance, flight.OriginCityName, flight.DestinationCityName }).Take(1);
 
             /*
             At first I had trouble accessing the properties of the anonymous type (shortestFlight) created by the linq statement above. 
-            I didn't want to use a foreach statement for a sequence with just one object.
-            However, all of the methods like FirstOrDefault(), Single() (and even more) below can do that:
+            (I didn't want to use a foreach statement for a sequence with just one object.)
+            However, all of the methods like FirstOrDefault(), Single() (and even more) below can access the (first instance) of an anonymous sequence:
             */
             Console.WriteLine("Query6:\nThe shortest flight was from {0} to {1} \nat a distance of {2} miles.\n", shortestFlight.FirstOrDefault().OriginCityName, shortestFlight.Single().DestinationCityName, shortestFlight.ElementAt(0).Distance);
 
@@ -87,7 +80,6 @@ namespace SheCodesMod7FlightDelaysLinqJk
 
             // Query8. The weighted arrival delay of a flight is its arrival delay divided by the distance. What
             // was the flight with the largest weighted arrival delay out of Boston, MA?  
-            
             //var weighted = (from flight in airlinePerformance2014
             //                where flight.OriginCityName == "Boston MA"
             //                orderby (flight.ArrivalDelay / flight.Distance) descending
@@ -99,11 +91,11 @@ namespace SheCodesMod7FlightDelaysLinqJk
             //    weighted.FirstOrDefault().Distance);
 
             /* 
-            My solution above worked. However, SheCodes used the let keyword:
+            My solution above worked. However, SheCodes used the "let" keyword:
             From DotNetPerls:
             "Let is a part of a query expression. It introduces a variable. We can then reuse the variable elsewhere in the query. 
             This makes possible certain complex query expressions, and makes other expressions simpler."
-            In debugging mode, the "weighted" variable is hidden in the "Autos" window under "Non Public members". 
+            Note: In debugging mode, the "weighted" variable is hidden in the "Autos" window under "Non Public members". 
             */
 
             // Query8. From Shecodes solution:
@@ -120,13 +112,11 @@ namespace SheCodesMod7FlightDelaysLinqJk
                 weighted.FirstOrDefault().Distance);
 
             // Query9. How many flights from Seattle, WA were not delayed on arrival?
-
             /*
             I made a pretty big mistake here when I used flight.ArrivalDelay == 0 at first.
-            This left out hundreds of flights that arrived early!! Shecodes used flight.ArrivalDelay <= 0
+            This left out hundreds of flights that arrived early!! Shecodes correctly uses flight.ArrivalDelay <= 0
             as below:
             */
-
             var undelayedFromSeattle = (from flight in airlinePerformance2014
                                         where flight.OriginCityName == "Seattle WA"
                                         where flight.ArrivalDelay <= 0
@@ -134,7 +124,6 @@ namespace SheCodesMod7FlightDelaysLinqJk
             Console.WriteLine("\nQuery9\n:{0} flights from Seattle were not delayed at all.", undelayedFromSeattle);
 
             // Query10 What were the top 10 origin airports with the largest average departure delays, including the values of these delays?
-
             var top10AverageDelayedAirports = (from flight in airlinePerformance2014
                                               group flight.DepartureDelay by flight.OriginCityName into departureDelayGroup
                                               let averageDelay = departureDelayGroup.Average()
@@ -145,8 +134,7 @@ namespace SheCodesMod7FlightDelaysLinqJk
             foreach (var airport in top10AverageDelayedAirports)
                 Console.WriteLine("Airport {0,-25} had an average delay of {1,-5:#.#} minutes.", airport.Airport, airport.Delay);
 
-            // Query11: (*) What is the airline with the worst average arrival delay on flights from New York, NY? 
-
+            // Query11: What is the airline with the worst average arrival delay on flights from New York, NY? 
             var worstAverageDelayAirlineFromNY = (from flight in airlinePerformance2014
                                                  where flight.OriginCityName == "New York NY"
                                                  group flight.ArrivalDelay by flight.Carrier into arrivalDelayCarrierGroup
@@ -158,7 +146,6 @@ namespace SheCodesMod7FlightDelaysLinqJk
 
             // Query 12: Which origin airport is the worst to fly from in terms of average departure delay if you are flying American Airlines
             // (airline code: "AA")?
-
             var worstOriginByAverageDepartureArrayForAA = (from flight in airlinePerformance2014
                                                            where flight.Carrier == "AA"
                                                            group flight.DepartureDelay by flight.OriginCityName into departureDelayOrigins
@@ -169,18 +156,16 @@ namespace SheCodesMod7FlightDelaysLinqJk
             Console.WriteLine("\nQuery12:\nAA has an average departure delay of {0:#.##} minutes\nwhen flying out of {1}.",
                 worstOriginByAverageDepartureArrayForAA.ElementAtOrDefault(0).Delay, worstOriginByAverageDepartureArrayForAA.ElementAtOrDefault(0).Origin);
 
-            // If I want handy access to elements in a list, rather than a sequence like above, I can convert using .ToList():
+            // If I want handy access to elements in a list (which I can access through an index []) I can convert using .ToList():
             var query12list = worstOriginByAverageDepartureArrayForAA.ToList();
             Console.WriteLine("\nQuery12\nOn average, AA departs {0} with a delay of {1:#.##} minutes.", query12list[0].Origin, query12list[0].Delay);
 
             // Query13: For all the 1 - stop flights between Boston, MA and Los Angeles, CA (not including direct flights), what was the flight path with the lowest
             // average arrival and departure delay ? (Note that this query can take a very long time to run.)
-
             /*
-            The following, awquard first try had run for 21 minute without results when I stopped it. I assume this could have something to do with linq
+            The following, aqward first try had run for 21 minute without results when I stopped it. I assume this could have something to do with linq
             queries running (each time) when their results are called.
             */
-
             //var query13a = from flight in airlinePerformance2014
             //               where flight.OriginCityName == "Boston MA"
             //               where flight.DestinationCityName != "Los Angeles CA"
@@ -208,15 +193,8 @@ namespace SheCodesMod7FlightDelaysLinqJk
             //    query13d.ElementAtOrDefault(0).flight.DestinationCityName, query13d.ElementAtOrDefault(0).averageDelay);
 
             /*
-            Trying to speed it up by storing the query results to lists. The following code took 9 secondes to run and displayed:
-            // Query13:
-            // Flights from Boston to Los Angeles via Phoenix AZ had an average arrival and dep
-            // arture delay(combined) of - 4,26052906325294E-12.
-
-            When I fix the division mistake below the result turns to:
-            Query13:
-            Flights from Boston to Los Angeles via Phoenix AZ had an average arrival and dep
-            arture delay (combined) of -1,6512839248542E-05.
+            Trying to speed it up by storing the query results to lists. The code below fixed a division error I made and by producing
+            preliminary results rather than calling the same queries over and over again. It takes between 9 and 30 seconds to run on my system.
             */
 
             var query13a = from flight in airlinePerformance2014
@@ -246,18 +224,18 @@ namespace SheCodesMod7FlightDelaysLinqJk
                             let averageDepartureDelay = (double)flight.DepartureDelay / query13cList.Count()
                             let averageDelay = (averageArrivalDelay + averageDepartureDelay) /// query13cList.Count()
                             orderby averageDelay
+                            // selecting a number of variables for debugging purposes:
                             select new { flight, arrivalDelay, departureDelay, averageArrivalDelay, averageDepartureDelay, relevantFlights, averageDelay }
                             ).Take(1);
             var query13dList = query13d.ToList();
 
-            // I am puzzled as to why my custom numeric format strings "1:#.##" cause the value (negative double) to not be displayed:
-            // The debugger shows it as "-1.651283924854197E-05".
-            // Try formatting it in the select clause
-            //Console.WriteLine("\nQuery13:\nFlights from Boston to New york via {0} had an average arrival and departure delay (combined) of {1:#.##}.",
-            //    query13d.ElementAtOrDefault(0).flight.DestinationCityName, query13d.ElementAtOrDefault(0).averageDelay);
-
             Console.WriteLine("\nQuery13:\nFlights from Boston to Los Angeles via {0} had an average arrival and departure delay (combined) of {1}.",
                 query13d.ElementAtOrDefault(0).flight.DestinationCityName, query13d.ElementAtOrDefault(0).averageDelay);
+
+            // I am puzzled as to why my custom numeric format strings "1:#.##" cause the value (negative double) to not be displayed:
+            // The debugger shows it as "-1.651283924854197E-05".
+            //Console.WriteLine("\nQuery13:\nFlights from Boston to New york via {0} had an average arrival and departure delay (combined) of {1:#.##}.",
+            //    query13d.ElementAtOrDefault(0).flight.DestinationCityName, query13d.ElementAtOrDefault(0).averageDelay);
 
 
             /* 
@@ -266,17 +244,6 @@ namespace SheCodesMod7FlightDelaysLinqJk
             Boston MA to Los Angeles CA via Tampa FL had average
             combined arrival/departure delay of -24,3113821138211
             */
-
-
-            //var query13test = from leg1 in airlinePerformance2014
-            //                  where leg1.OriginCityName == "Boston MA"
-            //                  from leg2 in airlinePerformance2014
-            //                  where leg2.DestinationCityName == "Los Angeles CA"
-            //                  where leg2.OriginCityName == leg1.DestinationCityName
-            //                  select new { leg1dd = leg1.DepartureDelay, leg1ad = leg1.ArrivalDelay, leg2dd = leg2.DepartureDelay, leg2ad = leg2.ArrivalDelay };
-            //var quer13testList = query13test.ToList();
-
-
 
             //var query13 = (from leg1 in airlinePerformance2014
             //               where leg1.OriginCityName == "Boston MA"
@@ -294,35 +261,16 @@ namespace SheCodesMod7FlightDelaysLinqJk
             //Console.WriteLine("{0} to {1} via {2} had average\ncombined arrival/departure delay of {3}",
             //    query13List[0].FlightLegs.O1, query13List[0].FlightLegs.D, query13List[0].FlightLegs.O2, query13List[0].averageTotalDelay);
 
-            /* 
-            Use lambda expressions to query the data.
-            Firstly, I want to select all flights from New York to Los Angeles.
-            Then get the sum of their delays.
-            Divide that sum by the number of flights (double value, instead of int)
-            Display average delay
+            // Testing some of their results in the debugger:
+            //var query13test = from leg1 in airlinePerformance2014
+            //                  where leg1.OriginCityName == "Boston MA"
+            //                  from leg2 in airlinePerformance2014
+            //                  where leg2.DestinationCityName == "Los Angeles CA"
+            //                  where leg2.OriginCityName == leg1.DestinationCityName
+            //                  select new { leg1dd = leg1.DepartureDelay, leg1ad = leg1.ArrivalDelay, leg2dd = leg2.DepartureDelay, leg2ad = leg2.ArrivalDelay };
+            //var quer13testList = query13test.ToList();
 
-            After that make a prompt and take in the user values to do the calculation.
-            Check with test file (remember that weeded out lines will throw this off a bit!)
-            */
-
-            // Using a LINQ expression:
-            //var newYorkToLAFlights = from flightInfo in airlinePerformance2014
-            //                         where flightInfo.OriginCityName == "New York NY"
-            //                         && flightInfo.DestinationCityName == "Los Angeles CA"
-            //                         select flightInfo;
-            //Console.WriteLine("Average Flight Arrival Delay: " + (newYorkToLAFlights.Sum(p => p.ArrivalDelay) / ((double)newYorkToLAFlights.Count())));
-
-            // Using a lambda expression:
-            //var newYorkToLAFlights = airlinePerformance2014.Where(p => p.OriginCityName == "New York NY").Where(p => p.DestinationCityName == "Los Angeles CA");
-            //Console.WriteLine("Average Flight Arrival Delay: " + (newYorkToLAFlights.Sum(p => p.ArrivalDelay) / ((double)newYorkToLAFlights.Count())));
-
-
-            //double delaySum = newYorkToLAFlights.Sum(p => p.ArrivalDelay);
-            //Console.WriteLine(delaySum);
-            //Console.WriteLine((double)newYorkToLAFlights.Count());
-
-
-           auConsole.ReadLine();
+            Console.ReadLine();
         }
     }
 }
